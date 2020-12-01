a95a9c9066037fcd2cec6975c53e30568d6d95e7
bigname <tuanhiep.tran88@gmail.com>
2020-11-30 22:46:11 -0700

    Finish testing for greedy approach and dynamic programming approach

--- src/main/java/lab5/DynamicProgramming.java
+++ src/main/java/lab5/DynamicProgramming.java
@@ -1,37 +1,63 @@
 package lab5;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class DynamicProgramming {
 
 
     public static void main(String[] args) {
-        // Example 1
-        System.out.println("* Example 1");
+        System.out.println("I>TEST FUNCTION");
+        // Test 1
+        System.out.println("* Test 1");
         int[] w = new int[]{4, 5, 7};
         int[] v = new int[]{2, 3, 4};
         int m = 10;
         printData(w, v, m);
-        knapSackDp1(w, v, m);
-        knapSackDp2(w, v, m);
+        knapSackDp(w, v, m);
         knapSackGa(w, v, m);
 
-        // Example 2
-        System.out.println("* Example 2");
-        w = new int[]{2, 3, 3};
-        v = new int[]{3, 1, 7};
-        m = 6;
+        // Test 2
+        System.out.println("* Test 2");
+        w = new int[]{6, 5, 7, 3, 1};
+        v = new int[]{7, 3, 4, 4, 3};
+        m = 13;
         printData(w, v, m);
-        knapSackDp1(w, v, m);
-        knapSackDp2(w, v, m);
+        knapSackDp(w, v, m);
         knapSackGa(w, v, m);
+
+        // Test 3
+        System.out.println("* Test 3");
+        w = new int[]{2, 3, 5, 5, 3, 7};
+        v = new int[]{3, 4, 10, 9, 6, 13};
+        m = 15;
+        printData(w, v, m);
+        knapSackDp(w, v, m);
+        knapSackGa(w, v, m);
+
+        // Test 4
+        System.out.println("* Test 4");
+        w = new int[]{10, 13, 17, 15};
+        v = new int[]{21, 17, 30, 23};
+        m = 30;
+        printData(w, v, m);
+        knapSackDp(w, v, m);
+        knapSackGa(w, v, m);
+
+        // Test 5
+        System.out.println("* Test 5");
+        w = new int[]{5, 4, 7, 6, 3, 4, 2, 1, 7, 6};
+        v = new int[]{3, 1, 3, 2, 1, 3, 2, 3, 1, 4};
+        m = 30;
+        printData(w, v, m);
+        knapSackDp(w, v, m);
+        knapSackGa(w, v, m);
+        System.out.println("II> RUNTIME COMPARISON");
 
     }
 
     /**
      * print out given data
+     *
      * @param w
      * @param v
      * @param m
@@ -55,7 +81,7 @@ public class DynamicProgramming {
     }
 
     private static void knapSackGa(int[] w, int[] v, int m) {
-        System.out.println("3.Greedy approach:");
+        System.out.println("2.Greedy approach:");
         Item[] items = new Item[w.length];
 
         for (int i = 0; i < w.length; i++) {
@@ -106,40 +132,19 @@ public class DynamicProgramming {
      * @param maximum value
      * @return
      */
-    public static void knapSackDp1(int[] w, int[] v, int maximum) {
+    public static void knapSackDp(int[] w, int[] v, int maximum) {
         // total number of items
         int n = w.length;
-        // Solution 1
-        System.out.println("1.Tabulation - Bottom-up approach:");
-        int[][] knapSack1 = initialize(maximum, n);
-        int result = knapSackDp1(w, v, maximum, n - 1, knapSack1);
-        outputSelected1(w, v, maximum, n - 1, knapSack1, result);
-
-    }
-
-    /**
-     * Dynamic programming to find the maximum value with given constraint in weight
-     *
-     * @param w:      item weights
-     * @param v:      item values
-     * @param maximum value
-     * @return
-     */
-    public static void knapSackDp2(int[] w, int[] v, int maximum) {
-        // total number of items
-        int n = w.length;
-        // Solution 2
-        System.out.println("2.Memoization - Top-down approach:");
-        int[][] knapSack2 = initialize(maximum, n);
-        List<Integer> selected = new ArrayList<>();
-        int result2 = knapSackDp2(w, v, maximum, n - 1, knapSack2, selected);
-        outputSelected2(w, v, selected, result2);
+        System.out.println("1.Dynamic Programming - Tabulation - Bottom-up approach:");
+        int[][] knapSack = initialize(maximum, n);
+        int result = knapSackDp(w, v, maximum, n - 1, knapSack);
+        outputSelected(w, v, maximum, n - 1, knapSack, result);
 
     }
 
 
     /**
-     * Solution 1: Dynamic Programming algorithm to find maximum value
+     * Dynamic Programming algorithm to find maximum value
      *
      * @param w:         item weights
      * @param v:         item values
@@ -148,7 +153,7 @@ public class DynamicProgramming {
      * @param knapSack
      * @return maximum value
      */
-    private static int knapSackDp1(int[] w, int[] v, int maxWeight, int indexItem, int[][] knapSack) {
+    private static int knapSackDp(int[] w, int[] v, int maxWeight, int indexItem, int[][] knapSack) {
         int i, c;
         // Build table knapSack in bottom up manner
         for (i = 0; i <= indexItem; i++) {
@@ -176,7 +181,7 @@ public class DynamicProgramming {
      * @param knapSack
      * @param result
      */
-    private static void outputSelected1(int[] w, int[] v, int maxWeight, int indexItem, int[][] knapSack, int result) {
+    private static void outputSelected(int[] w, int[] v, int maxWeight, int indexItem, int[][] knapSack, int result) {
         if (result == 0) {
             System.out.println("No items was chosen !");
             return;
@@ -185,78 +190,20 @@ public class DynamicProgramming {
         int j = maxWeight;
         int i = indexItem;
         while (remProfit > 0) {
-            while (i > 0 && knapSack[i][j] == knapSack[i - 1][j] && knapSack[i][j] != -1) {
+            while (i > 0 && knapSack[i][j] == knapSack[i - 1][j]) {
                 i = i - 1;
             }
             System.out.println("Item " + i + ": w = " + w[i] + " v = " + v[i]);
             remProfit = remProfit - v[i];
-            while (knapSack[i][j] == -1 || knapSack[i][j] > remProfit) {
+            if (i > 0) {
+                i = i - 1;
+            }
+            while (knapSack[i][j] > remProfit) {
                 j = j - 1;
             }
 
         }
         System.out.println("were chosen and the total value is " + result);
-    }
-
-
-    /**
-     * Solution 2: Dynamic Programming algorithm to find maximum value and list of selected items
-     *
-     * @param w
-     * @param v
-     * @param maxWeight
-     * @param indexItem
-     * @param knapSack
-     * @param selected
-     * @return
-     */
-    private static int knapSackDp2(int[] w, int[] v, int maxWeight, int indexItem, int[][] knapSack, List<Integer> selected) {
-        if (indexItem == -1 || maxWeight == 0) {
-            return 0;
-        }
-        if (knapSack[indexItem][maxWeight] != -1) {
-            return knapSack[indexItem][maxWeight];
-        }
-        if (w[indexItem] <= maxWeight) {
-            List<Integer> list1 = new ArrayList<>();
-            List<Integer> list2 = new ArrayList<>();
-            int option1 = v[indexItem] + knapSackDp2(w, v, maxWeight - w[indexItem], indexItem - 1, knapSack, list1);
-            int option2 = knapSackDp2(w, v, maxWeight, indexItem - 1, knapSack, list2);
-            if (option1 > option2) {
-                selected.add(indexItem);
-                selected.addAll(list1);
-                knapSack[indexItem][maxWeight] = option1;
-            } else {
-                selected.addAll(list2);
-                knapSack[indexItem][maxWeight] = option2;
-            }
-
-        } else {
-            List<Integer> list = new ArrayList<>();
-            knapSack[indexItem][maxWeight] = knapSackDp2(w, v, maxWeight, indexItem - 1, knapSack, list);
-            selected.addAll(list);
-        }
-        return knapSack[indexItem][maxWeight];
-    }
-
-    /**
-     * Output the selected items for 0/1 knapSack problem
-     *
-     * @param w
-     * @param v
-     * @param selected
-     * @param total
-     */
-    private static void outputSelected2(int[] w, int[] v, List<Integer> selected, int total) {
-        if (selected.size() == 0) {
-            System.out.println("No items was chosen !");
-            return;
-        }
-
-        for (int item : selected) {
-            System.out.println("Item " + item + ": w = " + w[item] + " v = " + v[item]);
-        }
-        System.out.println("were chosen and the total value is " + total);
     }
 
 }
