8836c03f5a18c76196c404572ee402bc8d5cdf0b
tuanhiep <tuanhiep.tran.us@gmail.com>
2026-04-24 15:34:48 -0700

    defer() in WebFlux

--- src/main/java/lab6/MonoDefer.java
+++ src/main/java/lab6/MonoDefer.java
@@ -16,26 +16,23 @@ public class MonoDefer {
         Map<String, String> mdcContext = MDC.getCopyOfContextMap();
 
         return Mono.defer(() -> {
-                    if (mdcContext != null) MDC.setContextMap(mdcContext);
+            if (mdcContext != null) MDC.setContextMap(mdcContext);
 
-                    long now = System.currentTimeMillis();
-                    String freshSignature = sign(req, now);
+            long now = System.currentTimeMillis();
+            String freshSignature = sign(req, now);
 
-                    WebClient webClient = WebClient.create("http://partner-api.com/payment");
-                    return webClient.post().header("X-Timestamp", String.valueOf(now)).header("X-Signature",
-                                    freshSignature).bodyValue(req) // Đảm bảo req là POJO tĩnh, không phải Stream
-                            .retrieve().bodyToMono(PaymentResponse.class);
+            WebClient webClient = WebClient.create("http://partner-api.com/payment");
+            return webClient.post().header("X-Timestamp", String.valueOf(now)).header("X-Signature", freshSignature)
+                    .bodyValue(req) // Đảm bảo req là POJO tĩnh, không phải Stream
+                    .retrieve().bodyToMono(PaymentResponse.class);
 
-                })
-                .timeout(Duration.ofSeconds(5))
-                .retryWhen(Retry.backoff(3, Duration.ofSeconds(2)).filter(throwable -> {
-                    if (throwable instanceof WebClientResponseException) {
-                        WebClientResponseException ex = (WebClientResponseException) throwable;
-                        return ex.getStatusCode().is5xxServerError(); // Bỏ qua 4xx
-                    }
-                    return throwable instanceof TimeoutException || throwable instanceof IOException;
-                }))
-                .doFinally(signalType -> MDC.clear()); // Dọn dẹp bộ nhớ Log
+        }).timeout(Duration.ofSeconds(5)).retryWhen(Retry.backoff(3, Duration.ofSeconds(2)).filter(throwable -> {
+            if (throwable instanceof WebClientResponseException) {
+                WebClientResponseException ex = (WebClientResponseException) throwable;
+                return ex.getStatusCode().is5xxServerError(); // Bỏ qua 4xx
+            }
+            return throwable instanceof TimeoutException || throwable instanceof IOException;
+        })).doFinally(signalType -> MDC.clear()); // Dọn dẹp bộ nhớ Log
     }
 
     private String sign(PaymentRequest req, long now) {
